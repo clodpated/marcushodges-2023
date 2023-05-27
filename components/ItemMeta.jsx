de841093394d5710a1e823e6266fb2f45557ee33
Marcus Hodges <marcush@ifit.com>
2023-05-27 13:55:27 -0400

    Prettier

--- components/ItemMeta.jsx
+++ components/ItemMeta.jsx
@@ -4,9 +4,11 @@ export default function ItemMeta({ title, metadata }) {
   const joinedData = metadata.join(', ');
 
   return (
-    <div className='flex-1 flex-shrink-0'>
-      <h3 className='border-b border-neutral-900 text-lg font-semibold mb-1 uppercase'>{title}</h3>
-      <p className='text-xl mb-[1em]'>{joinedData}</p>
+    <div className="flex-1 flex-shrink-0">
+      <h3 className="border-b border-neutral-900 text-lg font-semibold mb-1 uppercase">
+        {title}
+      </h3>
+      <p className="text-xl mb-[1em]">{joinedData}</p>
     </div>
   );
 }
