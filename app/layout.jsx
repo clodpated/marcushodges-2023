149a32e58d1a194df6e76149ca0e1cda42b7c2c4
Marcus Hodges <marcush@ifit.com>
2023-05-28 13:07:16 -0400

    Add analytics

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,21 +1,43 @@
-import './globals.css'
-import { Raleway } from 'next/font/google'
+import Script from 'next/script';
+import { Raleway } from 'next/font/google';
+import './globals.css';
 
 const raleway = Raleway({
   weight: ['400', '600', '900'],
   subsets: ['latin'],
   display: 'swap',
-})
+});
 
 export const metadata = {
   title: 'Marcus Hodges - Maker of web things',
-  description: 'TODO',
-}
+  description:
+    'Portfolio and resume for Marcus Hodges: Front End / UI Developer and Engineering Manager',
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      {/* Google tag (gtag.js) */}
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-CQ4R1YHLWD"
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-CQ4R1YHLWD', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+
       <body className={`${raleway.className} antialiased`}>{children}</body>
     </html>
-  )
+  );
 }
